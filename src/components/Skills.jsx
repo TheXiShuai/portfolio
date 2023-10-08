846f260821bbe467deb09f636b201b6950cd8756
TheXiShuai <johndoe@example.com>
2023-10-08 18:27:51 +0200

    portfolio updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,6 +7,10 @@ import MONGO from '../assets/mongo.png'
 import NODE from '../assets/node.png'
 import REACT from '../assets/react.png'
 import BOOTSTRAP from '../assets/bootstrap.png'
+import HANDLEBARS from '../assets/handlebars.png'
+import NEXT from '../assets/next.png'
+import TYPESCRIPT from '../assets/typescript.png'
+import TAILWIND from '../assets/tailwind.png'
 
 const Skills = () => {
   return (
@@ -18,20 +22,16 @@ const Skills = () => {
             </div>
 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
     <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
-        <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-        <p className='my-4'>HTML</p>
-    </div>
-    <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
-        <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
-        <p className='my-4'>CSS</p>
-    </div>
-    <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
         <img className='w-20 mx-auto' src={JAVASCRIPT} alt="JAVASCRIPT icon" />
         <p className='my-4'>JAVASCRIPT</p>
     </div>
     <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
-        <img className='w-20 mx-auto' src={BOOTSTRAP} alt="BOOTSTRAP icon" />
-        <p className='my-4'>BOOTSTRAP</p>
+        <img className='w-20 mx-auto' src={TYPESCRIPT} alt="TYPESCRIPT icon" />
+        <p className='my-4'>TYPESCRIPT</p>
+    </div>
+    <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+        <p className='my-4'>HTML</p>
     </div>
     <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
         <img className='w-20 mx-auto' src={REACT} alt="REACT icon" />
@@ -42,12 +42,32 @@ const Skills = () => {
         <p className='my-4'>MONGO DB</p>
     </div>
     <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
-        <img className='w-20 mx-auto' src={NODE} alt="NODE icon" />
-        <p className='my-4'>NODE JS</p>
-    </div>
-    <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
         <img className='w-20 mx-auto' src={GITHUB} alt="GITHUB icon" />
         <p className='my-4'>GITHUB</p>
+    </div>
+    <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={NODE} alt="NODE icon" />
+        <p className='my-4'>NODE</p>
+    </div>
+    <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={NEXT} alt="NEXT icon" />
+        <p className='my-4'>NEXT</p>
+    </div>
+    <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={TAILWIND} alt="TAILWIND icon" />
+        <p className='my-4'>TAILWIND</p>
+    </div>
+    <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={BOOTSTRAP} alt="BOOTSTRAP icon" />
+        <p className='my-4'>BOOTSTRAP</p>
+    </div>
+    <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+        <p className='my-4'>CSS</p>
+    </div>
+    <div className=' shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={HANDLEBARS} alt="HANDLEBARS icon" />
+        <p className='my-4'>HANDLEBARS</p>
     </div>
 </div>
         </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import pinned from "../assets/pinnedFiction.png";
 import wild from "../assets/wildxperience.png";
 import harry from "../assets/HARRY.png";
+import carhub from "../assets/carhub.png";
+import prs from "../assets/prs.png";
+import stack from "../assets/stack.png";
 
 const Work = () => {
   return (
@@ -17,16 +20,16 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* GRID ITEM */}
           <div
-            style={{ backgroundImage: `url(${harry})` }}
+            style={{ backgroundImage: `url(${carhub})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* //HOVER EFECTS */}
             <div className="opacity-0 group-hover:opacity-80">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                JavaScript Game App
+              <span className="justify-center text-2xl font-bold text-white tracking-wider">
+                Typescript / React App
               </span>
               <div className="pt-8 text-center">
-                <a href="https://thexishuai.github.io/firstGameProject/">
+                <a href="https://carhub-vercel-caieyav0p-thexishuai.vercel.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
@@ -59,10 +62,68 @@ const Work = () => {
             {/* //HOVER EFECTS */}
             <div className="opacity-0 group-hover:opacity-80">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React App
+                Javascript / React App
               </span>
               <div className="pt-8 text-center">
               <a href="https://iridescent-panda-8f1d82.netlify.app/">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+                {/* CONTAINER gets up to 3 columns when need to add new work just copy the whole grid until I get 3 columns then copy the container(3 grids, 3 more projects) */}
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 pt-4">
+          {/* GRID ITEM */}
+          <div
+            style={{ backgroundImage: `url(${harry})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* //HOVER EFECTS */}
+            <div className="opacity-0 group-hover:opacity-80">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                JavaScript Game App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://thexishuai.github.io/firstGameProject/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${prs})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* //HOVER EFECTS */}
+            <div className="opacity-0 group-hover:opacity-80">
+              <span className="text-2xl font-bold text-white tracking-wider">
+              JavaScript Game App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://paper-scissors-rock-vercel.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${stack})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* //HOVER EFECTS */}
+            <div className="opacity-0 group-hover:opacity-80">
+              <span className="text-2xl font-bold text-white tracking-wider">
+              React Game App
+              </span>
+              <div className="pt-8 text-center">
+              <a href="https://extraordinary-sfogliatella-3ed53c.netlify.app/">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
