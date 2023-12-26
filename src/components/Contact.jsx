a062a0f3b7ee1f9c303b645e0652f1083afce3c6
TheXiShuai <johndoe@example.com>
2023-12-26 13:12:40 +0100

    update 2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,13 +6,17 @@ const Contact = () => {
       name="contact"
       className=" w-full lg:h-screen bg-[#0a192f] flex justify-center items-center p-4"
     >
-      <form method='POST' action="https://getform.io/f/9b7471c6-de03-415d-bf52-8052e023f561" className=" flex flex-col max-w-[600px] w-full">
+      <form
+        method="POST"
+        action="https://getform.io/f/9b7471c6-de03-415d-bf52-8052e023f561"
+        className=" flex flex-col max-w-[600px] w-full"
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
             Contact
           </p>
           <p className="text-gray-300 p-4">
-            Submit the form below or send me an email - martinbenitoh@gmail.com
+            Feel free to fill out the form below to reach me
           </p>
         </div>
         <input
